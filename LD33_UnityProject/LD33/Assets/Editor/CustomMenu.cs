c95aeec26824bf66d70608a0562014eb26a00057
Simon Albou <albou.simon@gmail.com>
2015-08-23 00:20:39 +0200

    Création du prefab BodyPartIcon en cours

---
+++ LD33_UnityProject/LD33/Assets/Editor/CustomMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CustomMenu : Editor {
+
+	[MenuItem("LD33/Serialize Things")]
+	public static void SerializeThings()
+	{
+		// donner TextBoxPool à BodyPartIcon
+		// donner les quatre BodyPartButton à BodyPartIcon
+	}
+}
