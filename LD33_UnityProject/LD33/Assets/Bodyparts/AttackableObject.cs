5cf50e8391d2dfd31ebc750ea6ee5aa731cf6d4f
Simon Albou <albou.simon@gmail.com>
2015-08-22 19:59:20 +0200

    Boutons latéraux, cooldown, début de système de combat

---
+++ LD33_UnityProject/LD33/Assets/Bodyparts/AttackableObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackableObject : MonoBehaviour {
+
+	public float maxHealth = 100;
+	private float curHealth;
+	public float atkMultiplier = 1;
+	public float defMultiplier = 1;
+
+	public Transform lifeBar;
+
+	void Start()
+	{
+		curHealth = maxHealth;
+	}
+
+	public void Hurt(float dmg)
+	{
+		curHealth -= dmg;
+		if (curHealth < 0) curHealth = 0;
+
+		lifeBar.localScale = new Vector3(curHealth / maxHealth, lifeBar.localScale.y, lifeBar.localScale.z);
+
+		if (curHealth == 0) Die();
+	}
+
+	public void Die()
+	{
+
+	}
+}
---
+++ LD33_UnityProject/LD33/Assets/Bodyparts/Bodypart.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bodypart : MonoBehaviour {
+
+	[Header("Battle Stats")]
+	public int slotNumber; // 0 = head, 1 = arms, 2 = misc, 3 = legs
+	public float cooldown;
+	public float damage; // base damage from this attack only
+	public float atkMultiplier; // atk bonus for all attacks
+	public float defMultiplier; // def bonus from all attacks
+	
+	[Header("Physic Stats")]
+	public float speedIncrease;
+	public float gravityIncrease;
+	public int extraAllowedJumps;
+	public bool affectsJumpCurve = false;
+	public AnimationCurve alternativeJumpCurve;
+	private AnimationCurve oldJumpCurve;
+
+	[Header("References")]
+	public SpriteRenderer playerGraphics;
+	public Animator playerAnimator;
+	public PlatformingInputHandler playerScript;
+	public AttackableObject playerBattleStats;
+
+	[Header("Graphics")]
+	public Sprite sprite;
+	public RuntimeAnimatorController animatorController;
+
+
+
+	public virtual void OnJustObtained()
+	{
+		playerGraphics.sprite = sprite;
+		playerAnimator.runtimeAnimatorController = animatorController;
+
+		playerBattleStats.atkMultiplier *= atkMultiplier;
+		playerBattleStats.defMultiplier *= defMultiplier;
+		playerScript.baseSpeed += speedIncrease;
+		playerScript.baseGravity += gravityIncrease;
+		playerScript.maxAllowedJumps += extraAllowedJumps;
+
+		if (affectsJumpCurve)
+		{
+			oldJumpCurve = playerScript.gravityJumpCurve;
+			playerScript.gravityJumpCurve = alternativeJumpCurve;
+		}
+	}
+
+	public virtual void OnUsingBodypart()
+	{
+
+	}
+
+	public virtual void OnJustLost()
+	{
+		playerBattleStats.atkMultiplier /= atkMultiplier;
+		playerBattleStats.defMultiplier /= defMultiplier;
+
+		playerScript.baseSpeed -= speedIncrease;
+		playerScript.baseGravity -= gravityIncrease;
+		playerScript.maxAllowedJumps -= extraAllowedJumps;
+
+		if (affectsJumpCurve)
+		{
+			playerScript.gravityJumpCurve = oldJumpCurve;
+		}
+	}
+}
