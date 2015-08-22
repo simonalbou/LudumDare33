26e2271d0d9fed4d31915e068a6ee4932f36a7e6
Sundro <magixmushroom@gmail.com>
2015-08-23 01:48:28 +0200

    Intégration du background de l'environnement psy

---
+++ LD33_UnityProject/LD33/Assets/Environnements/Psy/Scripts/Background.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour 
+{
+    public GameObject bordGauche;
+    public GameObject bordDroit;
+    private Transform trBG;
+    private Transform trBD;
+
+    private Transform tr;
+
+	// Use this for initialization
+	void Start () 
+    {
+        tr = transform;
+        trBG = bordGauche.transform;
+        trBD = bordDroit.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (tr.position.x < trBG.position.x)
+        {
+
+        }
+	}
+}
