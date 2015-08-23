3bd0b2c0358b9679fdea2e03a994a27cf3d20287
Sundro <magixmushroom@gmail.com>
2015-08-23 17:41:29 +0200

    Rajout du comportement walk des ennemis

--- LD33_UnityProject/LD33/Assets/Environnements/Psy/Scripts/ComportementEnnemis.cs
+++ LD33_UnityProject/LD33/Assets/Environnements/Psy/Scripts/ComportementEnnemis.cs
@@ -12,29 +12,38 @@ public class ComportementEnnemis : MonoBehaviour
     private bool attack;
     public float baseSpeed;
     public float attackSpeed;
+    public float distanceAggro;
+    private bool droite;
+    public float offset;
 
+    private CharacterController2D cc2d;
 
 	// Use this for initialization
 	void Start () 
     {
         self = transform;
 	    positionBase = transform.position;
+        if (walk)
+        {
+            cc2d = GetComponent<CharacterController2D>();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
+        if (Vector2.Distance(player.position, self.position) < distanceAggro)
+        {
+            attack = true;
+        }
+        else
+        {
+            attack = false;
+        }
+
+
         if (fly)
         {
-            if (Vector2.Distance(player.position, self.position) < 3f)
-            {
-                attack = true;
-            }
-            else
-            {
-                attack = false;
-            }
-
             Quaternion angleRotation;
             float speed;
             if (attack)
@@ -48,10 +57,10 @@ public class ComportementEnnemis : MonoBehaviour
                 speed = baseSpeed;
             }
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, angleRotation, Time.deltaTime * speed);
-            transform.GetChild(0).rotation = Quaternion.LookRotation(Vector3.forward, new Vector3(0f, 0f, 0f));
+            self.rotation = Quaternion.Slerp(self.rotation, angleRotation, Time.deltaTime * speed);
+            self.GetChild(0).rotation = Quaternion.LookRotation(Vector3.forward, new Vector3(0f, 0f, 0f));
 
-            transform.Translate(transform.up * Time.deltaTime * speed, Space.World);
+            self.Translate(self.up * Time.deltaTime * speed, Space.World);
         }
 
         if (longRange)
@@ -61,7 +70,35 @@ public class ComportementEnnemis : MonoBehaviour
 
         if (walk)
         {
-
+            if (attack)
+            {
+                cc2d.Move(new Vector2(Mathf.Sign(self.position.x - player.position.x)*-attackSpeed*Time.deltaTime, 0f));
+            }
+            else
+            {
+                if (droite)
+                {
+                    if (self.position.x < positionBase.x + offset)
+                    {
+                        cc2d.Move(new Vector2(Mathf.Sign(self.position.x - player.position.x) * baseSpeed * Time.deltaTime, 0f));
+                    }
+                    else
+                    {
+                        droite = false;
+                    }
+                }
+                else
+                {
+                    if (self.position.x > positionBase.x - offset)
+                    {
+                        cc2d.Move(new Vector2(Mathf.Sign(self.position.x - player.position.x) * -baseSpeed * Time.deltaTime, 0f));
+                    }
+                    else
+                    {
+                        droite = true;
+                    }
+                }
+            }
         }
 	}
 }
