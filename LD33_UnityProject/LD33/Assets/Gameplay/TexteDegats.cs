86e004b61ad38238c8999d5f7bd20c977c6c3e1d
Sundro <magixmushroom@gmail.com>
2015-08-24 23:12:45 +0200

    Effet de texte de dégat

--- LD33_UnityProject/LD33/Assets/Gameplay/TexteDegats.cs
+++ LD33_UnityProject/LD33/Assets/Gameplay/TexteDegats.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class TexteDegats : MonoBehaviour 
 {
-    public float speed;
-    public float time;
-    private float timer = 0f;
-
     private TextMesh txtmsh;
     private Transform self;
     private Color clr;
+
+    [Header("Vitesse Verticale")]
+    public float speed;
+    private float timer = 0f;
+
+    [Header("Changement scale")]
+    public float minimumScale = 0.0f;
+    public float maximumScale = 1f;
+    public float durationScale = 5.0f;
+    private float startTimeScale;
+    private bool canChangeAlpha = false;
+    public AnimationCurve curveScale;
+
+    [Header("Changement alpha")]
+    public float minimum = 0.0f;
+    public float maximum = 1f;
+    public float duration = 5.0f;
+    private float startTimeAlpha;
+    public AnimationCurve curveAlpha;
+
 
 	// Use this for initialization
 	void Start () 
@@ -17,18 +33,37 @@ public class TexteDegats : MonoBehaviour
         txtmsh = GetComponent<TextMesh>();
         self = transform;
         clr = txtmsh.color;
+
+        startTimeScale = Time.time;
+
+        //StartCoroutine(FadeOut(time));
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-	    if (timer <= time)
+        if (!canChangeAlpha)
         {
-            float offset = (Time.deltaTime/timer);
-            clr = txtmsh.color;
-            txtmsh.color = new Color(clr.r, clr.g, clr.b, (clr.a - offset));
-            timer += Time.deltaTime;
-            self.position = new Vector2(self.position.x, self.position.y + speed * Time.deltaTime);
+            float time = (Time.time - startTimeAlpha) / durationScale;
+            self.localScale = new Vector2(Mathf.Lerp(minimumScale, maximumScale, curveScale.Evaluate(time)), Mathf.Lerp(minimumScale, maximumScale, curveScale.Evaluate(time)));
+            if (time >= 1f)
+            {
+                startTimeAlpha = Time.time;
+                canChangeAlpha = true;
+            }
         }
+
+        if (canChangeAlpha)
+        {
+            float t = (Time.time - startTimeAlpha) / duration;
+            txtmsh.color = new Color(clr.r, clr.g, clr.b, Mathf.Lerp(minimum, maximum, curveAlpha.Evaluate(t)));
+        }
+
+	    if (timer <= duration + durationScale)
+        {
+            float delta = Time.deltaTime;
+            timer += delta;
+            self.position = new Vector2(self.position.x, self.position.y + speed * delta); 
+       }
 	}
 }
