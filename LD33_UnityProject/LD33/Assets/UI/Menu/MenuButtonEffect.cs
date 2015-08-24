6e5f0069d70a1c570f5482150f9f90e51c7ea696
Simon Albou <albou.simon@gmail.com>
2015-08-24 23:22:53 +0200

    Éléments de titlescreen + menu

---
+++ LD33_UnityProject/LD33/Assets/UI/Menu/MenuButtonEffect.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonEffect : MonoBehaviour {
+
+	public string sceneToLoad;
+	public GameObject objectToEnable;
+	public GameObject objectToDisable;
+	public bool quitsApplication;
+
+	public void ActivateButton()
+	{
+		if (objectToEnable) EnableGameObject();
+		if (objectToDisable) DisableGameObject();
+		if (sceneToLoad != "") LoadScene();
+		if (quitsApplication) QuitApplication();
+	}
+
+	public void LoadScene()
+	{
+		Application.LoadLevel(sceneToLoad);
+	}
+
+	public void EnableGameObject()
+	{
+		objectToEnable.SetActive(true);
+	}
+
+	public void DisableGameObject()
+	{
+		objectToDisable.SetActive(false);
+	}
+
+	public void QuitApplication()
+	{
+		Application.Quit();
+	}
+
+}
